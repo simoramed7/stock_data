164ee836a73c8b1ea5bd2cd56b388552309cfa67
Simo RAMI <sr-_-7@hotmail.com>
2021-06-12 00:36:21 +0100

    fetch and show data

--- src/components/stock-data/index.js
+++ src/components/stock-data/index.js
@@ -1,23 +1,23 @@
-import React,{useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import "./index.css";
 
 export default function StockData() {
-  const [data,setData]=useState([]);
-  const [input,setInput]=useState("");
+  const [data, setData] = useState([]);
+  const [input, setInput] = useState("");
   const [dateRequest, setDateRequest] = useState("");
-  
 
-const handleChange = event =>{
+
+  const handleChange = event => {
     setInput(event.target.value);
-}
+  }
 
-const handleSubmit = event =>{
-  event.preventDefault();
-  const dateLength = input.length;
-  input.indexOf(0) === 0 ? setDateRequest(input.slice(1,dateLength)) : setDateRequest(input);
-  
-}
- useEffect(() => {
+  const handleSubmit = event => {
+    event.preventDefault();
+    const dateLength = input.length;
+    input.indexOf(0) === 0 ? setDateRequest(input.slice(1, dateLength)) : setDateRequest(input);
+
+  }
+  useEffect(() => {
     const fetchFunc = async () => {
       const response = await fetch(
         `https://jsonmock.hackerrank.com/api/stocks?date=${dateRequest}`
@@ -26,30 +26,30 @@ const handleSubmit = event =>{
       setData(result.data[0]);
     };
 
-    if(dateRequest != ""){fetchFunc()}
+    if (dateRequest !== "") { fetchFunc() }
   }, [dateRequest]);
 
   return (
-    
+
     <div className="layout-column align-items-center mt-50">
       <section className="layout-row align-items-center justify-content-center">
-        <input type="text" className="large" placeholder="5-January-2000" onChange ={handleChange} id="app-input" data-testid="app-input"/>
+        <input type="text" className="large" placeholder="5-January-2000" onChange={handleChange} id="app-input" data-testid="app-input" />
         <button className="" id="submit-button" onClick={handleSubmit} data-testid="submit-button">Search</button>
       </section>
 
-    { dateRequest ?
-    data !== undefined ? 
-       <ul className="mt-50 slide-up-fade-in styled" id="stockData" data-testid="stock-data">
-       <li className="py-10">Open: {data.open} </li> 
-       <li className="py-10">Close: {data.close} </li> 
-       <li className="py-10">High: {data.high} </li> 
-       <li className="py-10">Low: {data.low} </li> 
-     </ul>
-    : 
-    <div className="mt-50 slide-up-fade-in" id="no-result" data-testid="no-result">No Results Found !!</div> : null
-    }
+      {dateRequest ?
+        data !== undefined ?
+          <ul className="mt-50 slide-up-fade-in styled" id="stockData" data-testid="stock-data">
+            <li className="py-10">Open: {data.open} </li>
+            <li className="py-10">Close: {data.close} </li>
+            <li className="py-10">High: {data.high} </li>
+            <li className="py-10">Low: {data.low} </li>
+          </ul>
+          :
+          <div className="mt-50 slide-up-fade-in" id="no-result" data-testid="no-result">No Results Found !!</div> : null
+      }
 
-</div>
+    </div>
 
 
   );
